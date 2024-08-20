954b3bc2e6842fadac269cacf1049891bb46b9aa
Weshy Joe <josephwaweru96@gmail.com>
2024-08-20 18:05:19 +0300

    initial commit

---
+++ src/main/java/com/weshy/tinder_ai_backend/TinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weshy.tinder_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+}
